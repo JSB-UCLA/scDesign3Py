0e1c5bd1e54fa73590c980aa0d28fb9f42e2ef52
Di Liu <liudi654@gmail.com>
2023-12-20 00:14:14 +0800

    Add doc

--- test/get_data.R
+++ test/get_data.R
@@ -1,5 +1,5 @@
-filename <- "BATCH"
+filename <- "ATAC"
 
-sce <- readRDS(paste0("./test/test_data/rds/", filename, ".rds"))
+sce <- readRDS(paste0("./test_data/rds/", filename, ".rds"))
 
-sceasy::convertFormat(sce, from = "sce", to = "anndata", main_layer = "counts", outFile = paste0("./test/test_data/", filename, ".h5ad"))
+sceasy::convertFormat(sce, from = "sce", to = "anndata", main_layer = "counts", outFile = paste0("./test_data/", filename, ".h5ad"))
