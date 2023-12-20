81f7dc8bb24c2f330f56ac315a54c4ebca3631bd
Di Liu <liudi654@gmail.com>
2023-12-20 17:02:10 +0800

    Update doc

--- test/get_data.R
+++ test/get_data.R
@@ -1,5 +1,6 @@
-filename <- "ATAC"
+filename <- "SCGEMRNA"
 
-sce <- readRDS(paste0("./test_data/rds/", filename, ".rds"))
+sce  <- readRDS(paste0("./test_data/rds/", filename, ".rds"))
 
 sceasy::convertFormat(sce, from = "sce", to = "anndata", main_layer = "counts", outFile = paste0("./test_data/", filename, ".h5ad"))
+
